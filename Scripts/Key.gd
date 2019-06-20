4eff0da167f15f4d9ebbf19af279b15a9d5cbd50
Caio Rosa <caiolrosa1912@gmail.com>
2019-06-19 23:42:23 -0300

    Implement base key

---
+++ Scripts/Key.gd
@@ -0,0 +1,20 @@
+extends Unlockable
+
+class_name Key
+
+onready var KeySprite: Sprite = $CollisionArea/Sprite
+
+var _KEY_TYPE_TEXTURES = {
+	UNLOCKABLE_TYPE.BLUE:   load("res://Assets/Keys/platformPack_item013.png"),
+	UNLOCKABLE_TYPE.YELLOW: load("res://Assets/Keys/platformPack_item014.png"),
+	UNLOCKABLE_TYPE.GREEN:  load("res://Assets/Keys/platformPack_item015.png"),
+	UNLOCKABLE_TYPE.RED:    load("res://Assets/Keys/platformPack_item016.png")
+}
+
+func _on_body_entered(body: PhysicsBody2D):
+	if body is Player:
+		EventBroker.dispatch(EventBroker.KEY_COLLECTED_EVENT, [type])
+		queue_free()
+
+func _ready():
+	KeySprite.texture = _KEY_TYPE_TEXTURES[type]
---
+++ Scripts/Unlockable.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+class_name Unlockable
+
+enum UNLOCKABLE_TYPE { BLUE, YELLOW, GREEN, RED }
+
+export(UNLOCKABLE_TYPE) var type = UNLOCKABLE_TYPE.BLUE
