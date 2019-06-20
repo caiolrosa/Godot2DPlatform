6aa8ba63f5e562b5db436bc71e4ed1e86fd89d8e
Caio Rosa <caiolrosa1912@gmail.com>
2019-06-19 22:40:52 -0300

    Add door opening sound

--- Scripts/Door.gd
+++ Scripts/Door.gd
@@ -2,25 +2,33 @@ extends Node2D
 
 class_name Door
 
-onready var top_frame_sprite: Sprite = $CollisionArea/TopFrame
-onready var key_hole_frame_sprite: Sprite = $CollisionArea/KeyHoleFrame
+onready var TopFrameSprite: Sprite = $CollisionArea/TopFrame
+onready var KeyHoleFrameSprite: Sprite = $CollisionArea/KeyHoleFrame
+onready var DoorOpenAudioPlayer: AudioStreamPlayer2D = $DoorOpenAudioPlayer
 
 export(Texture) var top_frame_texture
 export(Texture) var key_hole_frame_texture
 export(Texture) var top_frame_open_texture
 export(Texture) var key_hole_open_texture
 
+var is_open = false
+
 func _on_body_entered(body: PhysicsBody2D):
-	if body is Player:
-		top_frame_sprite.texture = top_frame_open_texture
-		key_hole_frame_sprite.texture = key_hole_open_texture
+	if not is_open and body is Player:
+		_open()
 
 func _ready():
-	assert_textures_exist()
-	top_frame_sprite.texture = top_frame_texture
-	key_hole_frame_sprite.texture = key_hole_frame_texture
+	_assert_textures_exist()
+	TopFrameSprite.texture = top_frame_texture
+	KeyHoleFrameSprite.texture = key_hole_frame_texture
 
-func assert_textures_exist():
+func _open():
+	is_open = true
+	TopFrameSprite.texture = top_frame_open_texture
+	KeyHoleFrameSprite.texture = key_hole_open_texture
+	DoorOpenAudioPlayer.play()
+
+func _assert_textures_exist():
 	assert( top_frame_texture      != null and 
 			key_hole_frame_texture != null and 
 			top_frame_open_texture != null and
